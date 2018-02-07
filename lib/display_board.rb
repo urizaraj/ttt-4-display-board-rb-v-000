5b0a0b683b09e660fcc0ee0e1e0f45ea2e83b846
Alexander Urizar <urizaraj@gmail.com>
2018-02-07 21:30:53 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,10 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(board)
+  [0,3,6].each do |i|
+    puts " #{board[i]} | #{board[i + 1]} | #{board[i + 2]} "
+    if i != 6
+      puts '-----------'
+    end
+  end
+end
